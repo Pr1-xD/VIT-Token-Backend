0544f483a0a991cd90fa4c54e5b6585291bc452a
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2023-05-19 08:00:28 +0530

    Completed Token Mint

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,7 @@
 import { instance } from "../server.js";
 import crypto, { setEngine } from "crypto";
 import Token from '../contracts/Token.js';
+import { ethers } from "ethers";
 import { Payment } from "../models/paymentModel.js";
 
 const TOKEN_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
@@ -22,11 +23,11 @@ export const checkout = async (req, res) => {
 
 async function setBalance(newBalance) {
   if (newBalance>0) {
-      let url = "http://something-else.com:8546";
+      let url = "http://127.0.0.1:8545/";
       let provider = new ethers.providers.JsonRpcProvider(url);
 
       //signer needed for transaction that changes state
-      const signer = new ethers.Wallet(your_private_key_string, provider);
+      const signer = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider);
       console.log(signer)
       const contract = new ethers.Contract(TOKEN_ADDRESS, Token.abi, signer);
 
